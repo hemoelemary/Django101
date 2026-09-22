72d0ba7dbedfb395371c4edbea384e63ab0bb99f
Your Name <hemoelemary>
2026-09-22 22:28:11 +0300

    making the forms and messages

--- feed/urls.py
+++ feed/urls.py
@@ -1,8 +1,9 @@
 from django.urls.conf import path,include
-from .views import HomeView
+from .views import HomeView,Details,AddPost
 
 app_name='feed'
 urlpatterns= [
-    path('',HomeView.as_view(),name='home'),
-    
+    path('',HomeView.as_view(),name='home'),   
+    path('detail/<int:pk>/',Details.as_view(),name='details'),
+    path('post/',AddPost.as_view(),name='post')
 ]
--- feed/views.py
+++ feed/views.py
@@ -1,10 +1,35 @@
-from django.views.generic import TemplateView
+from django.contrib import messages
+from django.views.generic import TemplateView,DetailView,FormView
 from .models import Post
+from .forms import PostForm
+
 # Create your views here.
 class HomeView(TemplateView):
     template_name = 'index.html'
-    objects = Post.objects.all()
+    #descending order by id newest
+    objects = Post.objects.all().order_by('-id')
     def get_context_data(self, **kwargs):
         context= super().get_context_data(**kwargs)
         context['objects']=self.objects
         return context
+
+class Details(DetailView):
+    template_name='details.html'
+    model=Post
+
+class AddPost(FormView):
+    template_name='add.html'
+    form_class=PostForm
+    success_url='/'
+    def dispatch(self, request, *args, **kwargs):
+        #when it is about to render
+        self.request=request
+        return super().dispatch(request, *args, **kwargs)
+    def form_valid(self,form):
+        # print(form.cleaned_data['image'])
+        new_object=Post.objects.create(
+            text=form.cleaned_data['text'],
+            image=form.cleaned_data['image']
+        )
+        messages.add_message(self.request,messages.SUCCESS,'Succed post')
+        return super().form_valid(form)
